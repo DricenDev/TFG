0cf19ade6e1eb3baf83a2bf329e24b0c61f63eb0
Valerio Luis <valeriouniversidad@gmail.com>
2025-05-08 17:21:28 +0100

    :tada: First commit

---
+++ CoinFlip/alice.py
@@ -0,0 +1,69 @@
+from qiskit                          import QuantumCircuit
+# from qiskit.compiler                 import transpile
+from qiskit.quantum_info             import Statevector
+# from qiskit.providers.basic_provider import BasicProvider, BasicSimulator
+# from qiskit.visualization            import plot_histogram, circuit_drawer, plot_bloch_multivector
+
+import random
+import json
+import socket
+
+# Constants
+NUM_QUBITS = 20
+
+# Step 1: Select a random base
+base = random.choice(['Computational', 'Hadamard'])
+print("Alice's base:")
+print(base)
+
+# Step 2: Select random bits
+bit_array = [str(random.randint(0, 1)) for _ in range(NUM_QUBITS)]
+print("Alice's random bits:")
+print(bit_array)
+
+# Step 3: Initialize each qubit
+qc_array = [QuantumCircuit(1) for _ in range(NUM_QUBITS)]
+for i in range(NUM_QUBITS):
+    if base == 'Computational':
+        if bit_array[i] == '1':
+            qc_array[i].x(0)
+    if base == 'Hadamard':
+        if bit_array[i] == '1':
+            qc_array[i].x(0)
+            qc_array[i].h(0)
+        else:
+            qc_array[i].h(0)
+
+# Step 4: Send qubits to Bob
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(('coin_flip_bob', 61000))
+    data_to_send = [Statevector(qc).data for qc in qc_array]
+    data_to_send = json.dumps(data_to_send).encode('utf-8')
+    s.sendall(data_to_send)
+
+# Step 5: Receive Bob's base
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('coin_flip_alice', 61000))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        data = conn.recv(1024)
+        bob_base = json.loads(data.decode('utf-8'))
+
+# Step 6: Send Bob the base
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(('coin_flip_bob', 61000))
+    data_to_send = json.dumps(bob_base).encode('utf-8')
+    s.sendall(data_to_send)
+
+# Step 7: Send Bob the bits
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(('coin_flip_bob', 61000))
+    data_to_send = json.dumps(bit_array).encode('utf-8')
+    s.sendall(data_to_send)
+
+# Step 8: You either won or not
+if base == bob_base:
+    print("Bob WON!")
+else:
+    print("Alice WON!")
---
+++ CoinFlip/bob.py
@@ -0,0 +1,66 @@
+from qiskit                          import QuantumCircuit
+# from qiskit.compiler                 import transpile
+from qiskit.quantum_info             import Statevector
+# from qiskit.providers.basic_provider import BasicProvider, BasicSimulator
+# from qiskit.visualization            import plot_histogram, circuit_drawer, plot_bloch_multivector
+
+import random
+import json
+import socket
+
+# Constants
+NUM_QUBITS = 20
+
+# Step 1: Select random bases
+base_array = [random.choice(['Computational', 'Hadamard']) for _ in range(NUM_QUBITS)]
+print("Bob's bases:")
+print(base_array)
+
+# Step 2: Receive qubits from Alice
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('coin_flip_bob', 61000))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        data = conn.recv(1024)
+        qc_array = json.loads(data.decode('utf-8'))
+        qc_array = [QuantumCircuit(1, 1).from_dict(qc) for qc in qc_array]
+
+# Step 3: Measure qubits
+for i in range(NUM_QUBITS):
+    if base_array[i] == 'Computational':
+        qc_array[i].measure(0, 0)
+    if base_array[i] == 'Hadamard':
+        qc_array[i].h(0)
+        qc_array[i].measure(0, 0)
+
+# Step 4: Select random base
+base = random.choice(['Computational', 'Hadamard'])
+print("Bob's guess base:")
+print(base)
+
+# Step 5: Send Alice the base
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(('coin_flip_alice', 61000))
+    data_to_send = json.dumps(base).encode('utf-8')
+    s.sendall(data_to_send)
+
+# Step 6: Receive Alice's base
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('coin_flip_bob', 61000))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        data = conn.recv(1024)
+        alice_base = json.loads(data.decode('utf-8'))
+
+# Step 7: Receive Alice's bits
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('coin_flip_bob', 61000))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        data = conn.recv(1024)
+        alice_bits = json.loads(data.decode('utf-8'))
+
+# Step 8: Compare bits
